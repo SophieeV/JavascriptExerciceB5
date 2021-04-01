39366fd6cccc1718341a64e84af8b23162944973
SophieeV <sophievesin@wanadoo.fr>
2021-04-01 10:31:48 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,20 @@
+//image1
+function departure1(element) {
+element.setAttribute('src', 'images/image1_2.jpg');
+}
+//image2
+function departure2(element) {
+element.setAttribute('src', 'images/image2_2.jpg');
+}
+//image 3
+function departure3(element) {
+element.setAttribute('src', 'images/image3_2.jpg');
+}
+//image5
+function departure4(element) {
+element.setAttribute('src', 'images/image4_2.jpg');
+}
+//image 5
+function departure5(element) {
+element.setAttribute('src', 'images/image5_2.jpg');
+}
